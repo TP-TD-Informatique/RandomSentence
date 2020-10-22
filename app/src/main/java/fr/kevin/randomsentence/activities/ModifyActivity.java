60d2fac41c753701ad740e481b7f9648020d3057
Kevin Traini <kevin@ktraini.com>
2020-10-22 09:14:08 +0200

    Set register name in textView + remove text in edit text when added

--- app/src/main/java/fr/kevin/randomsentence/activities/ModifyActivity.java
+++ app/src/main/java/fr/kevin/randomsentence/activities/ModifyActivity.java
@@ -3,11 +3,10 @@ package fr.kevin.randomsentence.activities;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.List;
 
 import fr.kevin.randomsentence.R;
 import fr.kevin.randomsentence.dialog.DeleteDialogFragment;
@@ -30,25 +29,18 @@ public class ModifyActivity extends AppCompatActivity implements Updatable {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify);
 
-        start();
+        registerJsonFileStorage = RegisterJsonFileStorage.get(getApplicationContext());
+        registerList = new RegisterList(registerJsonFileStorage.findAll());
 
         registerId = (Integer) getIntent().getSerializableExtra(REGISTER);
-
-        if (registerId == null) {
-            add();
-        } else {
-            register = registerList.get(registerId);
-            modify();
-        }
-    }
-
-    private void modify() {
-        findViewById(R.id.modify_database_name).setEnabled(false);
-        ((EditText) findViewById(R.id.modify_database_name)).setText(register.getName());
+        register = registerList.get(registerId);
+        ((TextView) findViewById(R.id.modify_database_name)).setText(register.getName());
         findViewById(R.id.modify_add_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String sentence = ((EditText) findViewById(R.id.modify_add_sentence)).getText().toString();
+                EditText add_sentence = findViewById(R.id.modify_add_sentence);
+                String sentence = add_sentence.getText().toString();
+                add_sentence.setText("");
                 register.add(sentence);
                 registerJsonFileStorage.update(registerId, register);
                 Toast.makeText(getApplicationContext(), R.string.modify_add_sentence_info, Toast.LENGTH_SHORT).show();
@@ -66,32 +58,6 @@ public class ModifyActivity extends AppCompatActivity implements Updatable {
                 (new DeleteDialogFragment(ModifyActivity.this, registerId)).show(getSupportFragmentManager(), "");
             }
         });
-    }
-
-    private void add() {
-        ((EditText) findViewById(R.id.modify_database_name)).setHint("Name ...");
-        findViewById(R.id.modify_add_btn).setEnabled(false);
-        findViewById(R.id.modify_add_sentence).setEnabled(false);
-
-        findViewById(R.id.modify_ok_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = ((EditText) findViewById(R.id.modify_database_name)).getText().toString();
-                registerJsonFileStorage.insert(new Register(name));
-                finish();
-            }
-        });
-        findViewById(R.id.modify_cancel_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-    }
-
-    private void start() {
-        registerJsonFileStorage = RegisterJsonFileStorage.get(getApplicationContext());
-        registerList = new RegisterList(registerJsonFileStorage.findAll());
     }
 
     @Override
